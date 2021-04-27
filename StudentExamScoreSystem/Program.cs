13290528fee5f60cc0c2c5a6bf700065b8a46bc3
Mahammad <devmahammad@gmail.com>
2021-04-27 21:29:22 +0400

    file structure clean up

--- StudentExamScoreSystem/Program.cs
+++ StudentExamScoreSystem/Program.cs
@@ -12,7 +12,7 @@ namespace StudentExamScoreSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new StudentExamScoreSys());
+            Application.Run(new LoginScreen());
         }
 
         
